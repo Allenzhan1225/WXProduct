fed421dc2da655ff1a2db2ffc42afeab71d94d93
Allenzhan1225 <1042748558@qq.com>
2017-07-03 15:32:35 +0800

    添加了colloctionView的图片

--- 连云港九龙小程序/pages/index/index.js
+++ 连云港九龙小程序/pages/index/index.js
@@ -16,10 +16,23 @@ Page({
     //列表数据
     list:[
       { 'img':'../../images/home/index_3.jpg','title':'前列腺疾病','detial':'如何判断自己患上了前列腺炎？','color':'rgb(14,119,205)'},
-      { 'img': '../../images/home/index_4.jpg', 'title': '性功能障碍', 'detial': '早泄检查项目有哪些', 'color': 'rgb(14,119,205)' },
-      { 'img': '../../images/home/index_5.jpg', 'title': '性传播疾病', 'detial': '梅毒的检查需要做哪些', 'color': 'rgb(14,119,205)' },
-      { 'img': '../../images/home/index_6.jpg', 'title': '生殖整形', 'detial': '欧式高频等离子包皮美容术', 'color': 'rgb(14,119,205)' },
+      { 'img': '../../images/home/index_4.jpg', 'title': '性功能障碍', 'detial': '早泄检查项目有哪些', 'color': 'rgb(250,150,49)' },
+      { 'img': '../../images/home/index_5.jpg', 'title': '性传播疾病', 'detial': '梅毒的检查需要做哪些', 'color': 'rgb(118,183,47)' },
+      { 'img': '../../images/home/index_6.jpg', 'title': '生殖整形', 'detial': '欧式高频等离子包皮美容术', 'color': 'rgb(47,48,233)' },
     ],
+    //列表数据
+    list2: [
+      { 'img': '../../images/home/index_8.jpg',},
+      { 'img': '../../images/home/index_9.jpg',},
+      { 'img': '../../images/home/index_10.jpg',},
+      { 'img': '../../images/home/index_11.jpg',},
+      { 'img': '../../images/home/index_12.jpg', },
+      { 'img': '../../images/home/index_13.jpg', },
+      { 'img': '../../images/home/index_14.jpg', },
+      { 'img': '../../images/home/index_15.jpg', },
+      { 'img': '../../images/home/index_16.jpg', },
+    ],
+
   },
   //事件处理函数
   bindViewTap: function() {
